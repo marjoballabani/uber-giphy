db29316385eb98b7e0b5d24d6e705b8e7b4a7236
Marjo <mballabani@apprisemobile.com>
2019-05-08 19:56:25 +0200

    fix typo

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -5,7 +5,8 @@ const Card = (props) => {
         <div className="card col-md-12 text-center img-wraper" key={props.img.id}>
             <img
                 className="card-img-top"
-                src={props.img.images.downsized_medium.url}img-container
+                img-container
+                src={props.img.images.downsized_medium.url}
                 alt={props.img.title}
             />
             <div className="card-body">
