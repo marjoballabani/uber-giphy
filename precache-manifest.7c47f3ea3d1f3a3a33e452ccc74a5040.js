bded69b9c5ab467bd3372c4107f0996233b9d543
Marjo <mballabani@apprisemobile.com>
2019-05-08 20:00:26 +0200

    demo

---
+++ precache-manifest.7c47f3ea3d1f3a3a33e452ccc74a5040.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58650b13dcdb6a9007c99aefb3ef3b3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6227c71e7cbecfa2992",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "122bfad9e2d6d3694547",
+    "url": "/static/css/main.26cc0210.chunk.css"
+  },
+  {
+    "revision": "d6227c71e7cbecfa2992",
+    "url": "/static/js/2.53a12186.chunk.js"
+  },
+  {
+    "revision": "122bfad9e2d6d3694547",
+    "url": "/static/js/main.5a0735e3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
